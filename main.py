8443c95f25079ad492c2b5aa47aed5167ce28c38
mason <lieberknechtmason@gmail.com>
2024-09-28 14:05:29 -0400

    Fix infer_signature issue in Random Forest model

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def go(config: DictConfig):
                 os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
                 "main",
                 parameters={
-                    "input_artifact": "sample.csv:latest",  # Specify the version
+                    "input_artifact": "sample.csv:latest",
                     "output_artifact": "clean_sample.csv",
                     "output_type": "clean_sample",
                     "output_description": "Data with outliers and null values removed",
@@ -67,8 +67,8 @@ def go(config: DictConfig):
                 os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
                 "main",
                 parameters={
-                    "csv": "clean_sample.csv:latest",  # Input CSV artifact
-                    "ref": "clean_sample.csv:reference",  # Reference CSV artifact
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
                     "kl_threshold": config["data_check"]["kl_threshold"],
                     "min_price": config["etl"]["min_price"],
                     "max_price": config["etl"]["max_price"],
@@ -80,10 +80,10 @@ def go(config: DictConfig):
                 f"{config['main']['components_repository']}/train_val_test_split",
                 "main",
                 parameters={
-                    "input": "clean_sample.csv:latest",  # Specify the version
+                    "input": "clean_sample.csv:latest",
                     "test_size": config["modeling"]["test_size"],
-                    "random_state": config["modeling"]["random_state"],
-                    "stratify": config["modeling"]["stratify_by"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
                 },
             )
 
@@ -93,7 +93,7 @@ def go(config: DictConfig):
             # Serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]['random_forest'].items()), fp)  # DO NOT TOUCH
+                json.dump(dict(config["modeling"]['random_forest'].items()), fp)
 
             # Run the train_random_forest step
             _ = mlflow.run(
@@ -101,10 +101,12 @@ def go(config: DictConfig):
                 "main",
                 parameters={
                     "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
                     "rf_config": rf_config,
-                    "output_artifact": config["modeling"]["output_artifact"],
                     "max_tfidf_features": config["modeling"]["max_tfidf_features"],
-                    "random_seed": config["modeling"]["random_state"]
+                    "output_artifact": "random_forest_export"
                 },
             )
 
@@ -117,6 +119,8 @@ def go(config: DictConfig):
 
 if __name__ == "__main__":
     go()
+
+
 
 
 
